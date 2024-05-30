b9e851138a2f8560341bbb6fded1364b7f55e8a4
Farmata Kane <kane.farmata@gmail.com>
2024-05-30 13:17:55 +0100

    Add logic for the toggle button

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const bodyColor = document.querySelector('body');
+const theNavContainer = document.querySelector('#nav');
+const menuBtn = document.querySelector('#menu__nav');
+const mainNav = document.querySelector('#main__nav');
+const navToggle = document.querySelector('.nav--toggle');
+
+menuBtn.addEventListener('click', () => {
+//  bodyColor.style.background = 'lime'
+    mainNav.classList.toggle('nav--toggle');
+    // alert('pressed');
+});
